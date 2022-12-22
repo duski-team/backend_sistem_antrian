5eb3697ee7b776ddde5247458a01af1d16d0d5a0
budi-irawan <budi.irawan171717@gmail.com>
2022-12-22 17:27:51 +0700

    reg

--- module/booking/controller.js
+++ module/booking/controller.js
@@ -1,91 +1,113 @@
 const booking = require('./model');
-const {sq} = require("../../config/connection");
+const { sq } = require("../../config/connection");
 const { v4: uuid_v4 } = require("uuid");
 const { QueryTypes } = require('sequelize');
-const s = {type:QueryTypes.SELECT}
+const s = { type: QueryTypes.SELECT }
 const antrian_list = require('../antrian_list/model')
 
 
-class Controller{
+class Controller {
 
-    static registerDenganRM(req,res){
-        const{tanggal_booking,jenis_booking,NIK,nama_booking,no_hp_booking,no_rujukan,no_kontrol,is_verified,is_registered,status_booking,rm_id,tanggal_antrian,poli_layanan,initial,jadwal_dokter_id,poli_id}=req.body
+    static registerDenganRM(req, res) {
+        const { tanggal_booking, jenis_booking, NIK, nama_booking, no_hp_booking, no_rujukan, no_kontrol, is_verified, is_registered, status_booking, rm_id, tanggal_antrian, poli_layanan, initial, jadwal_dokter_id, poli_id } = req.body
 
-        booking.create({id:uuid_v4(),tanggal_booking,jenis_booking,NIK,nama_booking,no_hp_booking,no_rujukan,no_kontrol,is_verified,is_registered,status_booking,rm_id})
-        .then(async hasil=>{   
-            let nomernya = await sq.query(`select count(*) from antrian_list al where date(al.tanggal_antrian) = '${tanggal_antrian}'and poli_id =${poli_id} and initial = '${initial}' and is_master=1`,s)
-               let nomer_antrian=+nomernya[0].count+1
-        
-    
-            const sequence = await sq.query(`select count(*) from antrian_list al where date(tanggal_antrian) = '${tanggal_antrian}' and poli_id =${poli_id} `,s)
-            // res.json('oke')
-    
-            console.log(nomer_antrian,sequence[0].count);
-    
-            antrian_list.create({id:uuid_v4(),tanggal_antrian,is_master:1,poli_layanan,initial,antrian_no:nomer_antrian,sequence:+sequence[0].count+1,jadwal_dokter_id,poli_id,booking_id:hasil.id})
-            .then(hasil=>{
-                res.status(200).json({ status: 200, message: "sukses",data:hasil})
+        booking.create({ id: uuid_v4(), tanggal_booking, jenis_booking, NIK, nama_booking, no_hp_booking, no_rujukan, no_kontrol, is_verified, is_registered, status_booking, rm_id })
+            .then(async hasil => {
+                let nomernya = await sq.query(`select count(*) from antrian_list al where date(al.tanggal_antrian) = '${tanggal_antrian}'and poli_id =${poli_id} and initial = '${initial}' and is_master=1`, s)
+                let nomer_antrian = +nomernya[0].count + 1
+
+
+                const sequence = await sq.query(`select count(*) from antrian_list al where date(tanggal_antrian) = '${tanggal_antrian}' and poli_id =${poli_id} `, s)
+                // res.json('oke')
+
+                console.log(nomer_antrian, sequence[0].count);
+
+                antrian_list.create({ id: uuid_v4(), tanggal_antrian, is_master: 1, poli_layanan, initial, antrian_no: nomer_antrian, sequence: +sequence[0].count + 1, jadwal_dokter_id, poli_id, booking_id: hasil.id })
+                    .then(hasil => {
+                        res.status(200).json({ status: 200, message: "sukses", data: hasil })
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        res.status(500).json({ status: 500, message: "gagal", data: error })
+                    })
+                res.status(200).json({ status: 200, message: "sukses", data: hasil })
             })
-            .catch(error=>{
-                console.log(error);
-                res.status(500).json({ status: 500, message: "gagal", data: error})
-            })
-            res.status(200).json({ status: 200, message: "sukses",data:hasil})
-        })
     }
 
+    static async registerTanpaRM(req, res) {
+        const { tanggal_booking, jenis_booking, NIK, nama_booking, no_hp_booking, no_rujukan, no_kontrol, is_verified, is_registered, status_booking, tanggal_antrian, poli_layanan, initial, jadwal_dokter_id, poli_id } = req.body
 
+        const t = await sq.transaction();
 
-    static update(req,res){
-        const{tanggal_booking,jenis_booking,NIK,nama_booking,no_hp_booking,no_rujukan,no_kontrol,is_verified,is_registered,status_booking,rm_id,id}=req.body
-
-        booking.update({id:uuid_v4(),tanggal_booking,jenis_booking,NIK,nama_booking,no_hp_booking,no_rujukan,no_kontrol,is_verified,is_registered,status_booking,rm_id},{where:{
-            id
-        }})
-        .then(hasil=>{
-            res.status(200).json({ status: 200, message: "sukses"})
-        })
-        .catch(error=>{
+        try {
+            let data_booking = await booking.create({ id: uuid_v4(), tanggal_booking, jenis_booking, NIK, nama_booking, no_hp_booking, no_rujukan, no_kontrol, is_verified, is_registered, status_booking }, { transaction: t })
+            let nomernya = await sq.query(`select count(*) from antrian_list al where date(al.tanggal_antrian) = '${tanggal_antrian}'and poli_id =${poli_id} and initial = '${initial}' and is_master = 1`, s)
+            let nomer_antrian = +nomernya[0].count + 1
+            const sequence = await sq.query(`select count(*) from antrian_list al where date(tanggal_antrian) = '${tanggal_antrian}' and poli_id =${poli_id} `, s)
+            
+            let data_antrian = await antrian_list.create({ id: uuid_v4(), tanggal_antrian, is_master: 1, poli_layanan, initial, antrian_no: nomer_antrian, sequence: +sequence[0].count + 1, jadwal_dokter_id, poli_id, booking_id: data_booking.id }, { transaction: t })
+            await t.commit();
+            res.status(200).json({ status: 200, message: "sukses", data: data_booking })
+        } catch (error) {
+            await t.rollback();
             console.log(error);
-            res.status(500).json({ status: 500, message: "gagal", data: error})
+            console.log(req.body);
+            res.status(500).json({ status: 500, message: "gagal", data: error })
+        }
+    }
+
+    static update(req, res) {
+        const { tanggal_booking, jenis_booking, NIK, nama_booking, no_hp_booking, no_rujukan, no_kontrol, is_verified, is_registered, status_booking, rm_id, id } = req.body
+
+        booking.update({ id: uuid_v4(), tanggal_booking, jenis_booking, NIK, nama_booking, no_hp_booking, no_rujukan, no_kontrol, is_verified, is_registered, status_booking, rm_id }, {
+            where: {
+                id
+            }
         })
+            .then(hasil => {
+                res.status(200).json({ status: 200, message: "sukses" })
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({ status: 500, message: "gagal", data: error })
+            })
 
     }
 
-    static async list(req,res){
-        const{tanggal_awal,tanggal_akhir,halaman,jumlah}=req.body
+    static async list(req, res) {
+        const { tanggal_awal, tanggal_akhir, halaman, jumlah } = req.body
 
         try {
             let isi = '';
-            let offset = (+halaman -1) * jumlah;
+            let offset = (+halaman - 1) * jumlah;
             let offset2 = +halaman * jumlah;
             let sisa = true;
 
-            if(tanggal_awal){
-                isi+=` and b.tanggal_booking >= ${tanggal_awal} `
+            if (tanggal_awal) {
+                isi += ` and b.tanggal_booking >= ${tanggal_awal} `
             }
-            if(tanggal_akhir){
-                isi+=` and b.tanggal_booking <= ${tanggal_akhir} `
+            if (tanggal_akhir) {
+                isi += ` and b.tanggal_booking <= ${tanggal_akhir} `
             }
 
-            let data = await sq.query(`select * from booking b where b."deletedAt" isnull ${isi} order by b.id desc limit ${jumlah} offset ${offset}`,s)
-            let data2 = await sq.query(`select * from booking b where b."deletedAt" isnull ${isi} order by b.id desc limit ${jumlah} offset ${offset2}`,s)
+            let data = await sq.query(`select * from booking b where b."deletedAt" isnull ${isi} order by b.id desc limit ${jumlah} offset ${offset}`, s)
+            let data2 = await sq.query(`select * from booking b where b."deletedAt" isnull ${isi} order by b.id desc limit ${jumlah} offset ${offset2}`, s)
 
-            let jml = await sq.query(`select count(*) from booking b where b."deletedAt" isnull ${isi} `,s)
+            let jml = await sq.query(`select count(*) from booking b where b."deletedAt" isnull ${isi} `, s)
 
-            if(data2.length==0){
+            if (data2.length == 0) {
                 sisa = false
             }
 
-            res.status(200).json({status:200,message:"sukses",data,count:jml[0].total,sisa,jumlah,halaman});  
+            res.status(200).json({ status: 200, message: "sukses", data, count: jml[0].total, sisa, jumlah, halaman });
         } catch (error) {
             console.log(error)
-            res.status(500).json({ status: 500, message: "gagal", data: error})
+            res.status(500).json({ status: 500, message: "gagal", data: error })
         }
 
-       
+
     }
 
 }
 
-module.exports=Controller
+module.exports = Controller
--- module/booking/route.js
+++ module/booking/route.js
@@ -2,8 +2,9 @@ const Controller = require('./controller')
 const router = require('express').Router()
 const authentification = require('../../middleware/authentification')
 
-router.post('/registerDenganRM',authentification, Controller.registerDenganRM)
-router.post('/update',authentification, Controller.update)
-router.post('/list',Controller.list)
+router.post('/registerDenganRM', authentification, Controller.registerDenganRM)
+router.post('/registerTanpaRM', authentification, Controller.registerTanpaRM)
+router.post('/update', authentification, Controller.update)
+router.post('/list', Controller.list)
 // router.post('/delete',authentification,Controller.delete)
 module.exports = router
