55511a45803cc7851f11ffc2d79769557fc6b909
faishalyafi <faishalyafiee@gmail.om>
2022-12-23 10:32:13 +0700

    fix

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const cors = require('cors')
 const routing = require('./routing/index')
 const server = require('http').createServer(app);
 const io = require('socket.io')(server,{cors:'*'});
+
 const antrian_loket= require('./module/antrian_loket/model')
+const booking = require('./module/booking/model');
+const { sq } = require("./config/connection");
+const { v4: uuid_v4 } = require("uuid");
+const { QueryTypes } = require('sequelize');
+const s = { type: QueryTypes.SELECT }
+const antrian_list = require('./module/antrian_list/model')
 
 
 
@@ -34,6 +41,49 @@ io.on('connection', function(socket) {
 			socket.emit("error",error);
         })
 	
+	})
+
+	socket.on('registerDenganRM',async (asd)=>{
+		const { tanggal_booking, jenis_booking, NIK, nama_booking, no_hp_booking, no_rujukan, no_kontrol, is_verified, is_registered, status_booking, rm_id, tanggal_antrian, poli_layanan, initial, jadwal_dokter_id, poli_id } = asd
+
+		const t = await sq.transaction();
+
+		try {
+			let nomernya = await sq.query(`select count(*) from antrian_list al where date(al.tanggal_antrian) = '${tanggal_antrian}'and poli_id =${poli_id} and initial = '${initial}' and is_master=1`, s)
+			let nomer_antrian = +nomernya[0].count + 1
+			const sequence = await sq.query(`select count(*) from antrian_list al where date(tanggal_antrian) = '${tanggal_antrian}' and poli_id =${poli_id} `, s)
+			let idBooking = uuid_v4()
+
+			await booking.create({ id: idBooking, tanggal_booking, jenis_booking, NIK, nama_booking, no_hp_booking, no_rujukan, no_kontrol, is_verified, is_registered, status_booking, rm_id },{transaction:t})
+			let antrian = await antrian_list.create({ id: uuid_v4(), tanggal_antrian, is_master: 1, poli_layanan, initial, antrian_no: nomer_antrian, sequence: +sequence[0].count + 1, jadwal_dokter_id, poli_id, booking_id: idBooking },{transaction:t})
+
+			await t.commit();
+			io.emit("refresh_loket",antrian);
+		} catch (error) {
+			await t.rollback();
+			socket.emit("error",error);
+		}
+	})
+
+	socket.on('registerTanpaRM',async (asd)=>{
+		const { tanggal_booking, jenis_booking, NIK, nama_booking, no_hp_booking, no_rujukan, no_kontrol, is_verified, is_registered, status_booking, tanggal_antrian, poli_layanan, initial, jadwal_dokter_id, poli_id } = asd
+
+        const t = await sq.transaction();
+
+        try {
+            let data_booking = await booking.create({ id: uuid_v4(), tanggal_booking, jenis_booking, NIK, nama_booking, no_hp_booking, no_rujukan, no_kontrol, is_verified, is_registered, status_booking }, { transaction: t })
+            let nomernya = await sq.query(`select count(*) from antrian_list al where date(al.tanggal_antrian) = '${tanggal_antrian}'and poli_id =${poli_id} and initial = '${initial}' and is_master = 1`, s)
+            let nomer_antrian = +nomernya[0].count + 1
+            const sequence = await sq.query(`select count(*) from antrian_list al where date(tanggal_antrian) = '${tanggal_antrian}' and poli_id =${poli_id} `, s)
+            
+            let data_antrian = await antrian_list.create({ id: uuid_v4(), tanggal_antrian, is_master: 1, poli_layanan, initial, antrian_no: nomer_antrian, sequence: +sequence[0].count + 1, jadwal_dokter_id, poli_id, booking_id: data_booking.id }, { transaction: t })
+            await t.commit();
+
+            io.emit("refresh_mobile",data_booking);
+        } catch (error) {
+            await t.rollback();
+			socket.emit("error",error);
+        }
 	})
 });
 
