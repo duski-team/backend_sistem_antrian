66e2225b52e11cb542254084b030ce8dee62bd3e
budi-irawan <budi.irawan171717@gmail.com>
2022-12-24 16:35:58 +0700

    user

--- module/users/route.js
+++ module/users/route.js
@@ -3,9 +3,12 @@ const router = require('express').Router()
 const authentification = require('../../middleware/authentification')
 
 router.post('/register', Controller.register)
-router.post('/update',authentification,Controller.update)
+router.post('/update', authentification, Controller.update)
 router.post('/login', Controller.login)
-router.get('/list',authentification,Controller.list)
-router.get('/detailsById/:id',authentification,Controller.detailsById)
-router.post('/delete',authentification,Controller.delete)
+router.get('/list', authentification, Controller.list)
+router.get('/detailsById/:id', authentification, Controller.detailsById)
+router.post('/delete', authentification, Controller.delete)
+router.post('/kirimOTP', Controller.kirimOTP)
+router.post('/changePasswordOTP', Controller.changePasswordOTP)
+
 module.exports = router
