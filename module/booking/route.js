2ae6406468b1ad702796e1cebac5cebd10df84cc
budi-irawan <budi.irawan171717@gmail.com>
2023-01-05 18:33:47 +0700

    ll

--- module/booking/route.js
+++ module/booking/route.js
@@ -4,7 +4,7 @@ const authentification = require('../../middleware/authentification')
 
 router.post('/registerDenganRM', authentification, Controller.registerDenganRM)
 router.post('/registerTanpaRM', authentification, Controller.registerTanpaRM)
-router.post('/update', authentification, Controller.update)
+router.post('/update', Controller.update)
 router.post('/list', authentification, Controller.list)
 router.post('/listBookingByUserId', Controller.listBookingByUserId)
 router.get('/detailsBookingByKodeBooking/:kode_booking', Controller.detailsBookingByKodeBooking)
