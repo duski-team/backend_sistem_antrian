ad6b9d6a37ab84c759fc0aa7f98759ffab643573
faishalyafi <faishalyafiee@gmail.om>
2023-01-08 10:58:03 +0700

    fix connection

--- src/config/connection.js
+++ src/config/connection.js
@@ -11,7 +11,7 @@ const sq = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     typeCast: true,
   },
   pool: {
-    max: 1000,
+    max: 300,
     min: 0,
     idle: 200000,
     acquire: 1000000,
