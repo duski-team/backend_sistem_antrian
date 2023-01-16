eac049a446a7ad8bf726d14d0b6e3db69f5c4891
faishalyafi <faishalyafiee@gmail.com>
2023-01-16 14:23:12 +0700

    fix rujukan

--- src/module/purworejo/route.js
+++ src/module/purworejo/route.js
@@ -11,6 +11,6 @@ router.post('/detailsPasienBPJS',Controller.detailsPasienBPJS)
 router.get('/detailsDataKontrol/:noSuratKontrol',Controller.detailsDataKontrol)
 router.post('/listRujukan',Controller.listRujukan)
 router.post('/getKontrol',Controller.getKontrol)
-router.get('/getListRujukInternal/:noRm',Controller.getListRujukInternal)
+router.post('/getListRujukInternal',Controller.getListRujukInternal)
 
 module.exports = router
