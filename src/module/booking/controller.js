20da68fd4bea268dbab1395dbcda1ccbb58ddf83
budi-irawan <budi.irawan171717@gmail.com>
2023-01-23 18:09:18 +0700

    l

--- src/module/booking/controller.js
+++ src/module/booking/controller.js
@@ -159,7 +159,7 @@ class Controller {
     static async detailsBookingByKodeBooking(req, res) {
         let { kode_booking } = req.params
         try {
-            let data = await sq.query(`select b.* , al.id as "antrian_list_id", al.tanggal_antrian ,al.is_master ,al.poli_layanan ,al.initial ,al.antrian_no ,al."sequence" ,al.is_cancel ,al.is_process ,al.status_antrian ,al.booking_id ,al.poli_id ,al.master_loket_id ,al.jenis_antrian_id from booking b left join antrian_list al on al.booking_id = b.id where b."deletedAt" isnull and b.kode_booking = '${kode_booking}'`, s)
+            let data = await sq.query(`select b.* , al.id as "antrian_list_id", al.tanggal_antrian ,al.is_master ,al.poli_layanan ,al.initial ,al.antrian_no ,al."sequence" ,al.is_cancel ,al.is_process ,al.status_antrian ,al.booking_id ,al.poli_id as "antrian_list_poli_id" ,al.master_loket_id ,al.jenis_antrian_id, jd.* from booking b join jadwal_dokter jd on jd.id = b.jadwal_dokter_id left join antrian_list al on al.booking_id = b.id where b."deletedAt" isnull and b.kode_booking = '${kode_booking}'`, s)
 
             res.status(200).json({ status: 200, message: "sukses", data })
         } catch (error) {
