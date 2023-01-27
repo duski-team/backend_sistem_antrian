3ea9966366d03c36be14c99d174b7b5b30a45644
faishalyafi <faishalyafiee@gmail.om>
2023-01-27 18:54:56 +0700

    fix

--- src/middleware/authentification.js
+++ src/middleware/authentification.js
@@ -6,13 +6,13 @@ async function authentification(req, res, next) {
     try {
         let decode = verifyToken(req.headers.token);
         // let role = ["1","2","4","5","6","7","8","9",'11',"24","99","1017","9998","9999"];
+        // console.log(decode);
         if (decode) {
             let data = await user.findAll({where: {username:decode.username}})
             req.dataUsers = decode
             if(data.length>0){
                 req.dataUsers = data[0]
             }
-
             next()
         }else {
             res.status(201).json({ status: 201, message: "anda belum login" });
