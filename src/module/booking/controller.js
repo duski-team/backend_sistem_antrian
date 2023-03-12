242ba1238eb69acb65f7901cc7618a66e0bc92f7
faishalyafi <faishalyafiee@gmail.com>
2023-03-12 17:42:00 +0700

    bookingAll updatedAt desc

--- src/module/booking/controller.js
+++ src/module/booking/controller.js
@@ -297,7 +297,7 @@ class Controller {
                 isi += ` date(jd.waktu_mulai)= '${tanggal_poli}'`
             }
 
-            let data = await sq.query(`select b.id as "booking_id", b.*,jd.*, u.username, u.user_status, u."role" from booking b left join jadwal_dokter jd on jd.id = b.jadwal_dokter_id left join users u on u.id = b.user_id where b."deletedAt" isnull ${isi}`, s)
+            let data = await sq.query(`select b.id as "booking_id", b.*,jd.*, u.username, u.user_status, u."role" from booking b left join jadwal_dokter jd on jd.id = b.jadwal_dokter_id left join users u on u.id = b.user_id where b."deletedAt" isnull ${isi} order by b."updatedAt" desc`, s)
 
             let kirim = await axios.get(purworejo + "/get-poli", config)
             let polinya = kirim.data.data
