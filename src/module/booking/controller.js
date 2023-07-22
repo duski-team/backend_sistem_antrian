a6aa8dd8a2913d6a9a1ce5f0c3c93ab067c70470
cipto <binarcipta02@gmail.com>
2023-07-22 10:46:11 +0700

    fix bug nama ada quote single

--- src/module/booking/controller.js
+++ src/module/booking/controller.js
@@ -101,7 +101,12 @@ class Controller {
     static async update(req, res) {
         const { id, tanggal_booking, jenis_booking, NIK, nama_booking, no_hp_booking, no_rujukan, no_kontrol, is_verified, is_registered, status_booking, no_rm, user_id, tujuan_booking, tanggal_rujukan, jadwal_dokter_id } = req.body
         if(status_booking == 2){
-            let asd = await sq.query(`select b.id as "booking_id", b.*,jd.*, u.username, u.user_status, u."role" from booking b left join jadwal_dokter jd on jd.id = b.jadwal_dokter_id left join users u on u.id = b.user_id where b.id = '${id}'`, s)
+            let asd = await sq.query(`select b.id as "booking_id", b.jenis_booking, b.jadwal_dokter_id, jd.waktu_mulai, 
+            jd.waktu_selesai, b.tanggal_booking, b.no_rm, b.no_hp_booking, b.kode_booking, b.flag_layanan, b.createdAt, 
+            jd.dokter_id, jd.poli_id
+            from booking b 
+            left join jadwal_dokter jd on jd.id = b.jadwal_dokter_id 
+            left join users u on u.id = b.user_id where b.id = '${id}'`, s)
             await axios.post(purworejo + "/kirim-booking", asd[0], config)
         }
 
